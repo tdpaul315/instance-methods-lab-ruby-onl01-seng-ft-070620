f79ded84b9f7013b10c43277d66b3914e06ce1c8
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-18 02:38:39 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+end
+ 
+fido = Dog.new
+
+class Dog
+  def bark
+    puts "Woof!"
+  end 
+end
+fido.bark 
+
+class Dog 
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+fido.sit 
+
+ 
+
+
+
