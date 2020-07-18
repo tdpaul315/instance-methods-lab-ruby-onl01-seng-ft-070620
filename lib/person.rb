91af7e450d4878dfdf8ae0602c46470395cc325e
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-18 03:06:55 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+end
+ 
+aida = Person.new
+
+class Person
+ def talk
+   puts "Hello World!"
+ end
+end
+aida.talk 
+
+class Person
+  def walk
+    puts "The Person is walking"
+  end
+end
+aida.walk 
