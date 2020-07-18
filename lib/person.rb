2299e907887f6a676b7146424da7a765afa28906
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-18 03:01:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,17 +3,3 @@ end
  
 aida = Person.new
 
-class Person
-  def talk
-    puts "Hello World!"
-  end 
-end
-person.talk 
-
-class Person
-  def walk
-    puts "The Person is walking"
-  end
-end
-person.walk
-
